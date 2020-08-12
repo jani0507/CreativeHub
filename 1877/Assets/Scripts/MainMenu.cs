25eb7b8852299cf2a3f9c29074252b89ac6a7001
jani0507 <69594245+jani0507@users.noreply.github.com>
2020-08-12 20:16:06 +0200

    Options menu

--- 1877/Assets/Scripts/MainMenu.cs
+++ 1877/Assets/Scripts/MainMenu.cs
@@ -1,22 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagment;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-       
-    
-    
-    // Start is called before the first frame update
-    void Start()
+    public GameObject mainCanvas;
+
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene("Game");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Debug.Log("Quitting Game");
+        Application.Quit();
+
     }
 }
